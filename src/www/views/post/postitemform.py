9dda7c6093b6469d6a53bbdaeb1c6b4f88cb5d34
Jacob Wan <jacob@jakewan.com>
2010-11-27 22:27:55 -0800

    Work on Post Item page

--- src/www/views/post/postitemform.py
+++ src/www/views/post/postitemform.py
@@ -7,8 +7,30 @@ class PostItemForm(RawStringComponent):
         super(PostItemForm, self).__init__(text=self.format_text())
         
     __formMarkup = '''\
-<form method="POST" action="/post">
+<form id="postItemForm" method="POST" action="/post">
     <div>
+        <div class="formRow">
+            <label for="shortDescription">Description</label>
+            <input id="shortDescription" name="shortDescription" class="niceInput"/>
+        </div>
+        
+        <fieldset id="locationRow">
+            <legend style="display: none">Location</legend>
+            
+            <div class="formRow">
+                <label for="lookupAddress">Address</label>
+                <input id="lookupAddress" name="lookupAddress" class="niceInput"/>
+                <button>Select</button>
+            </div>
+            
+            <div class="formRow">
+                <label for="longLat">Longitude, Latitude</label>
+                <input id="longLat" name="exactLongLat" class="niceInput"/>
+                <button>Select</button>
+                <em id="toggleMap" class="closed">Locate on map</em>
+            </div>
+        </fieldset>
+        
         <input type="submit" value="Post Item"/>
     </div>
 </form>
