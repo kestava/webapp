3ea41b9483d1712ad282d01991dcda3dae275353
Jacob Wan <jacob@jakewan.com>
2011-01-06 15:00:42 -0800

    Add log file rotation

--- src/www/app.py
+++ src/www/app.py
@@ -1,5 +1,7 @@
 import os.path
 import pprint
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 import cherrypy
 
@@ -18,17 +20,16 @@ def configure():
         'appSettings': {},
         'global': {
             'server.socket_host': settings.config['socket_host'],
-            'server.socket_port': settings.config['socket_port'],
-            'log.access_file': settings.config['access_log_path'],
-            'log.error_file': settings.config['error_log_path']
+            'server.socket_port': settings.config['socket_port']
         },
         '/': {
             'tools.gzip.on': True,
             'tools.gzip.mime_types': ['text/html', 'text/css', 'text/plain', 'application/javascript'],
             'tools.sessions.on': True,
-            'tools.sessions.name': 'KESTAVA_SESSION_COOKIE',
+            'tools.sessions.name': settings.config['session_cookie_name'],
             'tools.sessions.storage_type': 'file',
             'tools.sessions.storage_path': settings.config['session_storage_path'],
+            'tools.sessions.timeout': settings.config['session_timeout'],
             'tools.staticdir.root': staticDirectoryRoot,
             'tools.disconnect_db.on': True,
             'tools.trailing_slash.on': False
@@ -57,8 +58,38 @@ def configure():
         cherrypy.config.update({ 'environment': settings.config['environment'] })
     
     return serverConfig
+    
+def setup_logging():
+    '''Set up a TimedRotatingFileHandler'''
+    log = cherrypy.log
+    
+    # Remove the default FileHandlers if present.
+    log.error_file = ""
+    log.access_file = ""
 
+    h = TimedRotatingFileHandler(
+        filename=settings.config['error_log_path'],
+        when=settings.config['error_log_when'],
+        interval=settings.config['error_log_interval'],
+        backupCount=settings.config['error_log_backups'])
+    
+    h.setLevel(logging.DEBUG)
+    h.setFormatter(cherrypy._cplogging.logfmt)
+    log.error_log.addHandler(h)
+    
+    h = TimedRotatingFileHandler(
+        filename=settings.config['access_log_path'],
+        when=settings.config['access_log_when'],
+        interval=settings.config['access_log_interval'],
+        backupCount=settings.config['access_log_backups'])
+    
+    h.setLevel(logging.DEBUG)
+    h.setFormatter(cherrypy._cplogging.logfmt)
+    log.access_log.addHandler(h)
+    
 def main():
+    setup_logging()
+    
     cherrypy.quickstart(
         root=mapping.rootpages.RootPages(),
         config=configure()
