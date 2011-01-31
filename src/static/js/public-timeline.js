db7cb6a4178d899b8245977dc798c532d29d4a43
Jacob Wan <jacob@jakewan.com>
2011-01-30 19:59:41 -0800

    Get basic JSONP communication functioning.

--- src/static/js/public-timeline.js
+++ src/static/js/public-timeline.js
@@ -1,3 +1,18 @@
 // File: public-timeline.js
 
 var kestava;
+
+$(function() {
+    // Start loading the public timeline
+    // by default, we load the last 50 entries
+    kestava.beginLoadTimeline(50);
+});
+
+kestava.beginLoadTimeline = function(maxEntries) {
+    var url = 'http://timeline.kestava.jakewan.com/public?maxEntries=' + maxEntries + '&callback=?';
+    $.getJSON(
+        url,
+        function(result) {
+            alert(result);
+        });
+};
