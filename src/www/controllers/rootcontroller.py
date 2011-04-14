77118bba66e2263a47ba685e5e916aea3d9568d2
Jacob Wan <jacob@jakewan.com>
2011-04-14 11:02:16 -0700

    Work on top bar

--- src/www/controllers/rootcontroller.py
+++ src/www/controllers/rootcontroller.py
@@ -13,7 +13,9 @@ class RootController(object):
     @cherrypy.expose
     def index(self):
         env = cherrypy.request.app.jinjaEnv
-        template = env.get_template('html/homepage')
+        u = UserData()
+        templateName = 'html/{0}/homepage'.format(u.get_theme_name())
+        template = env.get_template(templateName)
         return template.render(
             siteData=SiteData(),
-            userData=UserData())
+            userData=u)
